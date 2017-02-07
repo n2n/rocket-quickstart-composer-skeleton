4a7e1d4ced33c93c0134ab477fd6c3ae50209cd9
bert <bert@BertHPi7-4.fritz.box>
2017-02-07 14:43:48 +0100

    added module with example solution to the project

---
+++ app/qs1/bo/BlogArticle.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace qs1\bo;
+
+use n2n\persistence\orm\CascadeType;
+use n2n\persistence\orm\annotation\AnnoOneToMany;
+use n2n\persistence\orm\annotation\AnnoManyToMany;
+use n2n\reflection\annotation\AnnoInit;
+use n2n\reflection\ObjectAdapter;
+use n2n\persistence\orm\annotation\AnnoOrderBy;
+
+class BlogArticle extends ObjectAdapter {
+	private static function _annos(AnnoInit $ai) {
+		$ai->p('comments', new AnnoOneToMany(BlogComment::getClass(), 'blogArticle', CascadeType::ALL));
+		$ai->p('comments', new AnnoOrderBy(array('id' => 'DESC')));
+		$ai->p('categories', new AnnoManyToMany(BlogCategory::getClass(), null, 
+				CascadeType::PERSIST|CascadeType::MERGE));
+	}
+	
+	private $id;
+	private $title;
+	private $lead;
+	private $contentHtml;
+	private $urlPart;
+	private $online;
+	private $comments;
+	private $categories;
+	
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+	public function getTitle() {
+		return $this->title;
+	}
+
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+
+	public function getLead() {
+		return $this->lead;
+	}
+
+	public function setLead($lead) {
+		$this->lead = $lead;
+	}
+
+	public function getContentHtml() {
+		return $this->contentHtml;
+	}
+
+	public function setContentHtml($contentHtml) {
+		$this->contentHtml = $contentHtml;
+	}
+
+	public function getUrlPart() {
+		return $this->urlPart;
+	}
+
+	public function setUrlPart($urlPart) {
+		$this->urlPart = $urlPart;
+	}
+
+	public function isOnline() {
+		return $this->online;
+	}
+
+	public function setOnline($online) {
+		$this->online = $online;
+	}
+	
+	/**
+	 * @return \qs\bo\BlogComment[]
+	 */
+	public function getComments() {
+		return $this->comments;
+	}
+	
+	/**
+	 * @param \qs\bo\BlogComment[]
+	 */
+	public function setComments(\ArrayObject $comments) {
+		$this->comments = $comments;
+	}
+	
+	/**
+	 * @return \qs\bo\BlogCategory[]
+	 */
+	public function getCategories() {
+		return $this->categories;
+	}
+	
+	/**
+	 * @param \qs\bo\BlogCategory[]
+	 */
+	public function setCategories(\ArrayObject $categories) {
+		$this->categories = $categories;
+	}
+
+}
